65959ca28cd61d9e5051cc83ee38a701013af780
eunse <eunse@DESKTOP-3CGC2VI>
2025-02-23 15:45:25 +0900

    초기 커밋.

---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapControllers();
+
+app.Run();
