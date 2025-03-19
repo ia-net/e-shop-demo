6ff486432a092c0970288de1e11a08c79cedb4fa
Lee <eunseokleedev@gmail.com>
2025-03-19 20:01:22 +0900

    end o section 11

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,12 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import  {provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient()
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))
   ]
 };
