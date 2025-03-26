434dcbc8e2604a6eec30bb6e61b08322da19ee64
myfavoritecolorisyou <eunseokleedev@gmail.com>
2025-03-26 20:45:05 +0900

    commit

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,12 +6,31 @@ import  {provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { InitService } from './core/services/init.service';
+import { lastValueFrom } from 'rxjs';
+
+function initializeApp(initService: InitService) {
+  return () => lastValueFrom(initService.init()).finally(() => {
+    console.log('APP_INITIALIZER 실행 완료');
+    const splash = document.getElementById('initial-splash');
+    if (splash) {
+      console.log('initial-splash 요소 제거');
+      splash.remove();
+    }
+  })
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])), 
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      multi: true,
+      deps: [InitService]
+    }
   ]
 };
